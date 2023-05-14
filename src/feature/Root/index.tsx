9e8ba5fef4b4d109e533231afcf271731bddf4da
binarybaba <amin.ajani17@gmail.com>
2023-05-14 23:12:23 +0200

    Fallback loading state

--- src/feature/Root/index.tsx
+++ src/feature/Root/index.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Navbar } from "src/feature/Navbar";
 import { AppProvider } from "src/context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const Root = () => {
   const queryClient = new QueryClient();
+  const navigation = useNavigation();
   return (
     <AppProvider>
       <QueryClientProvider client={queryClient}>
+        {navigation.state === "loading" && <div>LOADING</div>}
         <Navbar />
         <Outlet />
       </QueryClientProvider>
