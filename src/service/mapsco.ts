7936dfa8b85b4c8db3332365ca77acdd429b35e9
binarybaba <amin.ajani17@gmail.com>
2023-05-14 22:38:42 +0200

    use real api for reverse geocode

--- src/service/mapsco.ts
+++ src/service/mapsco.ts
@@ -13,23 +13,6 @@ export const fetchGeocodesByKeyword = async (
 export const fetchReverseGecode = async (
   coordinates: Coordinates
 ): Promise<MapsCoReverseGeocode> =>
-  new Promise((resolve) => {
-    resolve({
-      bounding_box: [1, 2, 3, 4],
-      class: "",
-      display_name: "location1",
-      importance: 1,
-      lat: 1,
-      lon: 1,
-      osm_id: "",
-      osm_type: "",
-      place_id: 1,
-      powered_by: "",
-      type: "",
-      address: {
-        country: "",
-        country_code: "",
-        place: "",
-      },
-    });
-  });
+  axios(
+    `${GEOCODE_SERVICE_URL}reverse?lat=${coordinates.lat}&lon=${coordinates.lon}`
+  ).then((res) => res.data);
