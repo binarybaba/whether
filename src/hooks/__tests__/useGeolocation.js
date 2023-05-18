ed4f6fcb17415c6b543be4a61c9366d85e6c468e
binarybaba <amin.ajani17@gmail.com>
2023-05-18 21:53:38 +0200

    Add useGeolocation test

--- src/hooks/__tests__/useGeolocation.js
+++ src/hooks/__tests__/useGeolocation.js
@@ -1,3 +1,32 @@
+import { useGeolocation } from "../useGeolocation";
+import { render, screen } from "@testing-library/react";
+
 describe("useGeolocation", () => {
-  it("should return with a location", () => {});
+  const mockGeolocation = {
+    getCurrentPosition: jest.fn().mockImplementation((success) =>
+      Promise.resolve(
+        success({
+          coords: {
+            latitude: 1111,
+            longitude: 2222,
+          },
+        })
+      )
+    ),
+  };
+  global.navigator.geolocation = mockGeolocation;
+  it("should return with a location", () => {
+    const Consumer = () => {
+      const geolocation = useGeolocation();
+      return (
+        <>
+          <div>Latitude: ${geolocation.coords.latitude}</div>
+          <div>Longitude: ${geolocation.coords.longitude}</div>
+        </>
+      );
+      render(<Consumer />);
+      expect(screen.getByText("Latitude:")).toHaveTextContent("1111");
+      expect(screen.getByText("Longitude:")).toHaveTextContent("2222");
+    };
+  });
 });
