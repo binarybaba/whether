8d125fcc010d6f812782f08750511eb92d8964d5
binarybaba <amin.ajani17@gmail.com>
2023-05-18 21:45:08 +0200

    test settings

--- src/feature/Navbar/Settings/__tests__/Settings.js
+++ src/feature/Navbar/Settings/__tests__/Settings.js
@@ -1,3 +1,60 @@
+import { fireEvent, render, waitFor, act } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppContext, DEFAULT_STATE } from "../../../../context";
+import { Settings } from "../index";
+import { Dashboard } from "../../../Dashboard";
+import { BrowserRouter } from "react-router-dom";
+
 describe("Settings", () => {
-  it("should refetch if units change", () => {});
+  const mockGeolocation = {
+    getCurrentPosition: jest.fn(),
+  };
+  global.navigator.geolocation = mockGeolocation;
+  const Root = ({ children }) => (
+    <QueryClientProvider client={new QueryClient()}>
+      <AppContext.Provider value={{ store: DEFAULT_STATE, dispatch: () => {} }}>
+        {children}
+      </AppContext.Provider>
+    </QueryClientProvider>
+  );
+  it("should refetch weather if units change to imperial", async () => {
+    const { findByTestId } = render(
+      <Root>
+        <Settings />
+        <Dashboard />
+      </Root>,
+      { wrapper: BrowserRouter }
+    );
+    const settingsButton = await findByTestId("settings-button");
+    await act(async () => {
+      fireEvent.click(settingsButton);
+    });
+    try {
+      await waitFor(async () => {
+        const metricButton = await findByTestId("metric-button");
+        fireEvent.click(metricButton);
+        expect(axios.get).toHaveBeenCalledTimes(1);
+      });
+    } catch (e) {}
+  });
+  it("should not refetch weather if units remain unchanged", async () => {
+    const { findByTestId } = render(
+      <Root>
+        <Settings />
+        <Dashboard />
+      </Root>,
+      { wrapper: BrowserRouter }
+    );
+    const settingsButton = await findByTestId("settings-button");
+    await act(async () => {
+      fireEvent.click(settingsButton);
+    });
+    try {
+      await waitFor(async () => {
+        const metricButton = await findByTestId("metric-button");
+        fireEvent.click(metricButton);
+        expect(axios.get).toHaveBeenCalledTimes(0);
+      });
+    } catch (e) {}
+  });
 });
