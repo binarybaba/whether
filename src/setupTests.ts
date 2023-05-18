24a24f2c69616f2ac020e4c549da56da3c422ec2
binarybaba <amin.ajani17@gmail.com>
2023-05-18 20:23:05 +0200

    override axios

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+export const mock = new MockAdapter(axios);
+
+mock.onGet("forecast").reply(200, {
+});
+
+mock.onGet("geocode").reply(200, {
+    users: [{ id: 1, name: "John Smith" }],
+});
